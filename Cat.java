9b3b374afb31e96a79d3f66092ff58c4c04ad957
Shangyue-CWU <zhush@cwu.edu>
2024-04-03 16:09:29 -0700

    Added a new java file Cat.java

---
+++ Cat.java
@@ -0,0 +1,19 @@
+
+public class TestSuite {
+	public static void main(String[] args) {
+        System.out.println("Hello, Cat!");
+
+    }
+
+}
+
+public class Cat{
+    private String name;
+    private int age;
+
+    public Cat(String name, int age){
+        this.age = age;
+        this.name = name;
+    }
+}
+
